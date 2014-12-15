ec372a9bcc077823a566a1ee501854931fe01be4
liyufeng <972008514@qq.com>
2014-12-15 12:03:13 +0800

    建立工程

---
+++ CarouselDiagram/CarouselDiagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CarouselDiagram
+//
+//  Created by liyufeng on 14/12/15.
+//  Copyright (c) 2014年 liyufeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CarouselDiagram/CarouselDiagramTests/CarouselDiagramTests.m
@@ -0,0 +1,40 @@
+//
+//  CarouselDiagramTests.m
+//  CarouselDiagramTests
+//
+//  Created by liyufeng on 14/12/15.
+//  Copyright (c) 2014年 liyufeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface CarouselDiagramTests : XCTestCase
+
+@end
+
+@implementation CarouselDiagramTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
